b791810ef0613780380794bc4e160f46ab5ffa15
ThOfficialCoder <sehzademar@outlook.com>
2025-08-23 20:18:25 -0400

    Conversion of Celsius to Fahrenheit implemented

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
-    println!("Hello, world!");
+    let result = celsius_fahrenheit(25);
+    println!("Result of conversion is {}F", result);
+}
+
+fn celsius_fahrenheit(number: u32) -> u32 {
+    let celsius = (number * 9/5) + 32;
+    return celsius;
 }
